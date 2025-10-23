148ab83d267a6139ed2661d4f632b69d1cbce79f
jsbaidwan <cgc.jsbaidwan@gmail.com>
2025-10-23 18:10:11 +0530

    Working on Guest layouts and also create a login api's for User and Super admin

--- frontend/src/components/Auth/SuperAdminLogin.js
+++ frontend/src/components/Auth/SuperAdminLogin.js
@@ -166,7 +166,7 @@ const SuperAdminLogin = () => {
                   id="password"
                   {...register('password')}
                   placeholder="Enter your password"
-                  className={`w-full pl-10 pr-5 text-sm sm:text-base text-black py-2 sm:py-2.5 border rounded-md focus:outline-none focus:ring-1 ${
+                  className={`w-full pl-10 pr-5 text-sm sm:text-base text-black py-2 sm:py-2.5 border rounded-tl-md rounded-bl-md focus:outline-none focus:ring-1 ${
                     errors?.password
                       ? 'border-red-500 focus:border-red-500 focus:ring-red-500'
                       : 'border-gray-300 focus:border-blue-500 focus:ring-blue-500'
