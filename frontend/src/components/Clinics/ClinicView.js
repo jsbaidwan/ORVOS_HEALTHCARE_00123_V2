e61ae9e6c7641091a35f5b812420879361b307dc
jsbaidwan <cgc.jsbaidwan@gmail.com>
2026-03-23 13:37:17 +0530

    Working on Clinic Module archive and filters on both pages clinic and archive listing.

--- frontend/src/components/Clinics/ClinicView.js
+++ frontend/src/components/Clinics/ClinicView.js
@@ -218,7 +218,7 @@ const ClinicView = () => {
                           key={`db-${index}`}
                           file={file}
                           index={index}
-                          onRemove=""
+                          onRemove={null}
                           onRemoveEnable={false}
                         />
                       )
