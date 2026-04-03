76d2d04f1e1e424bce8b860a8fac04486771ca1a
jsbaidwan <cgc.jsbaidwan@gmail.com>
2026-04-03 17:25:49 +0530

    Working on Patient Module, created create and update pages with working api's

--- backend/app/Helpers/Helper.php
+++ backend/app/Helpers/Helper.php
@@ -2827,7 +2827,7 @@ class Helper{
 		if($hasSigned){
 			return $signedUrl = \URL::signedRoute('file.serve', [
 			'token' => $token,
-			], now()->addSeconds(2));
+			], now()->addSeconds(8));
 		}else{
 			return $signedUrl = route('file.serve', ['token' => $token]);
 		}
