573f9c54b5997daff53d71e350c1db86975bc69d
jsbaidwan <cgc.jsbaidwan@gmail.com>
2026-07-01 18:37:39 +0530

    Working on pdftemplate froala edior issue.

--- frontend/src/components/Common/Header.js
+++ frontend/src/components/Common/Header.js
@@ -199,17 +199,38 @@ const Header = ({ toggleSidebar, closeSidebar }) => {
                       </div>
                     </Link>
                     <hr className="my-2 border-gray-200" />
-                    <button
-                      onClick={logout}
+
+                    {isImpersonated() ? (
+                      <>
+                      <button
+                      onClick={logoutImpersonation}
                       className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition-colors duration-200"
-                    >
-                      <div className="flex items-center space-x-2">
-                        <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                        </svg>
-                        <span>Logout</span>
-                      </div>
-                    </button>
+                      >
+
+                          <div className="flex items-center space-x-2">
+                            <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
+                            <span>Exit Account</span>
+                          </div>
+                      
+                      </button>
+                      </>
+                    ):(
+                      <>
+                        <button
+                          onClick={logout}
+                          className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition-colors duration-200"
+                        >
+                          <div className="flex items-center space-x-2">
+                            <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
+                            <span>Logout</span>
+                          </div>
+                        </button>
+                     </>
+                    )}
                   </div>
                 )}
               </div>
