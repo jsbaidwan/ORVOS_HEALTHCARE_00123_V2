964da4dc2d709cbba93eca9075ed8af06bc97033
jsbaidwan <cgc.jsbaidwan@gmail.com>
2026-08-10 17:56:27 +0530

    Working on Patient Module,change the Ui/UX of orvos doctor review according to screening type and adding the image carousel

--- frontend/src/components/Patients/PatientView.js
+++ frontend/src/components/Patients/PatientView.js
@@ -843,7 +843,7 @@ const PatientView = () => {
                                    <div className="grid grid-cols-1 md:grid-cols-12 gap-6 pt-3">
 
                                         {/* col-md-3 */}
-                                        <div className="col-span-12 md:col-span-2 space-y-6">
+                                        <div className="col-span-12 lg:col-span-2 md:col-span-3 space-y-6">
                                             <EyeImageSlider
                                                 label="Right Eye"
                                                 images={patient?.display_right_eye_images || []}
@@ -864,7 +864,7 @@ const PatientView = () => {
                                         </div>
 
                                         {/* col-md-5 */}
-                                        <div className="col-span-12 md:col-span-4">
+                                        <div className="col-span-12 lg:col-span-4 md:col-span-5">
                                             <EyeImageSlider
                                                 label="Both Eyes"
                                                 images={patient?.display_both_eye_images || []}
